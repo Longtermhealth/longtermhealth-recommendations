4a2ed7e35ce3a91ac18bafc8f9351e711c8f148d
jgLTH <j.grellner@longtermhealth.de>
2025-01-27 15:47:14 +0700

    update packages

--- scheduling/scheduler.py
+++ scheduling/scheduler.py
@@ -190,7 +190,7 @@ def load_routines_for_rules(file_path):
 
 
 def save_action_plan_json(final_action_plan,
-                          file_path='/Users/janoschgrellner/PycharmProjects/lth-recommendations/rule_based_system/data/action_plan.json'):
+                          file_path='./data/action_plan.json'):
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(final_action_plan, f, ensure_ascii=False, indent=2)
 
@@ -702,7 +702,7 @@ def main():
     else:
         gender = "MALE"
 
-    file_path = "/Users/janoschgrellner/PycharmProjects/lth-recommendations/rule_based_system/data/new_final_routines_with_scores.json"
+    file_path = "./data/routines_with_scores.json"
     routines = load_routines_for_rules(file_path)
 
     if isinstance(routines, dict):
@@ -720,7 +720,7 @@ def main():
     health_scores_with_tag = create_health_scores_with_structure(account_id, health_scores)
     print('health_scores_with_tag for posting:', json.dumps(health_scores_with_tag, indent=4, ensure_ascii=False))
 
-    packages_file_path = "/Users/janoschgrellner/PycharmProjects/lth-recommendations/rule_based_system/data/packages_with_id.json"
+    packages_file_path = "./data/packages_with_id.json"
     with open(packages_file_path, "r") as file:
         data = json.load(file)
     print('selected_packages', selected_packages)
