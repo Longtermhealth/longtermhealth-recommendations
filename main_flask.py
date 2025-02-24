df901f067fb1b2d08befcbb81c7012bd5af1333c
jgLTH <j.grellner@longtermhealth.de>
2025-02-24 18:22:48 +0700

    change event driven endpoint

--- main_flask.py
+++ main_flask.py
@@ -265,7 +265,7 @@ def event():
     host = request.host
     app.logger.info("Received webhook on host: %s", host)
     data = request.get_json()
-    print('data', data)
+    print("data", json.dumps(data, indent=4))
     if not data:
         return jsonify({"error": "No JSON payload provided"}), 400
 
