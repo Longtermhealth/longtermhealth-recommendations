b0ed317bccd77446576a152cbe222d4a3a235872
jgLTH <j.grellner@longtermhealth.de>
2025-03-03 14:51:15 +0700

    update eventbased webhook

--- main_flask.py
+++ main_flask.py
@@ -353,7 +353,8 @@ def event():
     host = request.host
     app.logger.info("Received webhook on host: %s", host)
     data = request.get_json()
-    print("data", json.dumps(data, indent=4, sort_keys=True))
+    print("data raw", data)
+    print("data", json.dumps(data, indent=4))
     if not data:
         return jsonify({"error": "No JSON payload provided"}), 400
 
